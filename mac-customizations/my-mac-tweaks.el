105ad4c865c8250914e4f79ad524ecdd7fc4dc33
Mike Breen <hardbap@gmail.com>
2013-02-06 09:52:43 -0500

    Fix PATH var for emacs.

--- mac-customizations/my-mac-tweaks.el
+++ mac-customizations/my-mac-tweaks.el
@@ -4,9 +4,6 @@
 ; delete files by moving them to the OS X trash
 (setq delete-by-moving-to-trash t)
 
-; setup environment variables from the user's shell
-; (exec-path-from-shell-initialize))
-
 ;; fix the PATH variable
 (defun set-exec-path-from-shell-PATH ()
   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
